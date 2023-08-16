b780678c189f54c5238b1dae12b071fa55e7352c
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-16 13:01:42 -0300

    feat: Adição do funcionamento do style.css na página

--- functions.php
+++ functions.php
@@ -4,5 +4,11 @@
         add_theme_support('woocommerce');
    }
    add_action('after_setup_theme', 'handel_add_woocommerce_support');
-   
+
+   function handel_add_style() {
+        wp_register_style('handel-style', get_template_directory_uri() . '/style.css', [], '1.0.0', false);
+        wp_enqueue_style('handel-style');
+   }
+   add_action('wp_enqueue_scripts', 'handel_add_style');
+
 ?>
