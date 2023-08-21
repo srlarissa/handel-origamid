dc35f37edf2fae102cee1eaf6663ed37076fbffc
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-21 11:35:31 -0300

    feat: Incremento na função de crop de imagem
    Agora a função também cria um tamanho de slide de 1000x800

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@
    add_action('wp_enqueue_scripts', 'handel_add_style');
 
    function handel_custom_images(){
+     add_image_size('slide', 1000, 800, ['center', 'top']);
      update_option('medium_crop', 1);
    }
    add_action('after_setup_theme', 'handel_custom_images');
