0d88139513379dde3d670c964c30ace212bb638e
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-22 11:10:18 -0300

    feat: Criação do menu de categorias

--- header.php
+++ header.php
@@ -33,3 +33,13 @@
         </a>
     </nav>
 </header>
+
+<?php 
+
+       wp_nav_menu([
+            'menu' => 'categorias',
+            'container' => 'nav',
+            'container_class' => 'menu-categorias'
+       ]);         
+
+?>
