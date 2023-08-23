432e816fa5edb066ccdf366ffdb24617ca146287
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-23 18:02:54 -0300

    feat: Tratamento dos dados para exibição do slide

--- page-home.php
+++ page-home.php
@@ -1,15 +1,42 @@
-<?php 
-    //Template Name: Home
-    get_header();
+    <?php 
+        //Template Name: Home
+        get_header();
+    ?>
+
+    <pre>
+        <?php
+            $products_slide = wc_get_products([
+                'limit' => 6,
+                'tag' => ['slide'],
+            ]);
+
+            function format_products($products){
+                $products_final=[];
+                foreach($products as $product){
+                    $products_final[] = [
+                        'name' => $product->get_name(),
+                        'preco' => $product->get_price_html(),
+                        'link' => $product->get_permalink(),
+                        'img' => wp_get_attachment_image_src()
+                    ];
+                }
+                return $products_final;
+            }
+
+            $slide = format_products($products_slide);
+
+            print_r($slide);
+        ?>
+    </pre>
+    <?php
 
             if(have_posts()) { while(have_posts()){ the_post();
-        ?>
+    ?>
 
-        <h1><?php the_title(); ?></h1>
-        <main><?php the_content(); ?></main>
+
 
     <?php
-        } }
+            } }
         
-    get_footer();
-?>
+        get_footer();
+    ?>
