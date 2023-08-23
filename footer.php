8acdc3045c13e05bdd3088cc33ac24f6b71bb884
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-23 18:02:25 -0300

    feat: Add js no footer

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+<script src="<?= get_stylesheet_directory_uri(); ?>/js/slide.js"></scrip>
+<script src="<?= get_stylesheet_directory_uri(); ?>/js/script.js"></script>
 <?php wp_footer(); ?> 
 </body>
 </html>
