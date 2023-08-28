531045262ecc65732b5e8c4b119081be8a02dcbc
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-08-28 16:53:27 -0300

    fix: Correção da posição do wp_footer

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
-<script src="<?= get_stylesheet_directory_uri(); ?>/js/slide.js"></scrip>
+
+<?php wp_footer(); ?>
+<script src="<?= get_stylesheet_directory_uri(); ?>/js/slide.js"></script>
 <script src="<?= get_stylesheet_directory_uri(); ?>/js/script.js"></script>
-<?php wp_footer(); ?> 
 </body>
 </html>
