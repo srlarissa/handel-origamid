bfd2ae26c0140057102ddccf5d05903c3caf3ead
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-09-01 15:13:40 -0300

    feat: Add filtro por preço e ordenação do WC

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -13,10 +13,46 @@
 <article class="container products-archive">
     <nav class="filtros">
         <h2>Filtros</h2>
+        <div class="filtro">
+            <h3 class="filtro-titulo">Categorias</h3>
+            <?php 
+                wp_nav_menu([
+                    'menu' => 'categorias-internas',
+                    'menu_class' => 'filtro-cat',
+                    'container' => false,
+                ]);
+            ?>
+        </div>
+        <div class="filtro">
+            <?php 
+                $attribute_taxonomies = wc_get_attribute_taxonomies();
+                foreach($attribute_taxonomies as $attribute){
+                    the_widget('WC_Widget_Layered_Nav', [
+                        'title' => $attribute->attribute_label,
+                        'attribute' => $attribute->attribute_name
+                    ]);
+                }
+            ?>
+        </div>
+        <div class="filtro">
+            <h3 class="filtro-titulo">Filtrar por preço</h3>
+            <form class="filtro-preco">
+                <div>
+                    <label for="min_price">De R$</label>
+                    <input required type="text" name="min_price" id="min_price" value="<?= $_GET['min_price'] ?? ''; ?>">
+                </div>
+                <div>
+                    <label for="max_price">Até R$</label>
+                    <input required type="text" name="max_price" id="max_price" value="<?= $_GET['max_price'] ?? ''; ?>">
+                </div>
+                <button type="submit">Filtrar</button>
+            </form>  
+        </div>
     </nav>
     <main>
         <?php 
             if($data['products']) {
+                woocommerce_catalog_ordering();
                 handel_product_list($data['products']);
                 echo get_the_posts_pagination(); 
             }else{ 
