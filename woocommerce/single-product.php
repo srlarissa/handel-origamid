29b35290e42211df271232b851efa218720b1f59
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-09-06 15:58:15 -0300

    feat: Imagens da galeria renderizadas em tela

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -17,19 +17,35 @@
             'link' => $product->get_permalink(),
             'sku' => $product->get_sku(),
             'description' => $product->get_description(),
-            'img' => w´_get_attachment_image_src($product->get_image_id(),$img_size)[0],
-            'gallery' => $gallery
+            'img' => wp_get_attachment_image_src($product->get_image_id(),$img_size)[0],
+            'gallery' => $gallery,
         ];
     }
 ?>
 
+    <div class="container breadcrumb">
+        <?php woocommerce_breadcrumb(['delimiter' => ' > ']); ?>
+    </div>   
+
+    <div class="container notificacao">
+        <?php wc_print_notices(); ?>
+    </div>
+
+    <main class="container product">
+
 <?php 
     if(have_posts()) {  while(have_posts()) {   the_post();
-        $product = handel_format_single_product(get_the_ID());
+        $produto = handel_format_single_product(get_the_ID());
 ?>
 
-        
-
 <?php } } ?>
+        <div class="product gallery" data-gallery="gallery">
+            <div class="product-gallery-list">
+                <?php foreach($produto['gallery'] as $img){ ?>
+                   <img data-gallery="list"src="<?= $img; ?>" alt="<?= $produto['name']?>"> 
+                <?php } ?>
+            </div>
+        </div>
+    </main>
 
 <?php   get_footer();   ?>
